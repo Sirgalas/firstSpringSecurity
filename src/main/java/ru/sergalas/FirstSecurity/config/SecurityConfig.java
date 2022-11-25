82918b7f93ab3b51f8949b1f54ca52e7fe175f39
sergalas <hudos@rambler.ru>
2022-11-25 19:15:31 +0300

    add auth config

--- src/main/java/ru/sergalas/FirstSecurity/config/SecurityConfig.java
+++ src/main/java/ru/sergalas/FirstSecurity/config/SecurityConfig.java
@@ -1,13 +1,16 @@
 package ru.sergalas.FirstSecurity.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import ru.sergalas.FirstSecurity.security.AuthProviderImp;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.sergalas.FirstSecurity.services.PersonDetailService;
 
 import javax.persistence.Entity;
 import java.net.Authenticator;
@@ -16,18 +19,41 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 public class SecurityConfig {
-    private final AuthProviderImp authProvider;
+    private final PersonDetailService personDetailService;
 
-    public SecurityConfig(AuthProviderImp authProvider)
+    @Autowired
+    public SecurityConfig(PersonDetailService personDetailService)
     {
-        this.authProvider = authProvider;
+        this.personDetailService = personDetailService;
     }
 
     @Bean
     @Primary
-    //настраивает аунтификатион
-    protected AuthenticationManagerBuilder configure(AuthenticationManagerBuilder auth)
+    //настраивает аунтификацию
+    protected AuthenticationManagerBuilder configure(AuthenticationManagerBuilder auth) throws Exception {
+        return auth.userDetailsService(personDetailService).and();
+    }
+
+    @Bean
+    //конфигурируеам сам  SpringSecurity
+    //и авторизацию
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/auth/login","/error").permitAll()
+            .anyRequest().authenticated().and()
+            .formLogin()
+            .loginPage("/auth/login")
+            .loginProcessingUrl("/process_login")
+            .defaultSuccessUrl("/hello",true)
+            .failureUrl("/auth/login?error");
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder()
     {
-        return auth.authenticationProvider(authProvider);
+        return NoOpPasswordEncoder.getInstance();
     }
 }
--- src/main/java/ru/sergalas/FirstSecurity/controllers/HelloController.java
+++ src/main/java/ru/sergalas/FirstSecurity/controllers/HelloController.java
@@ -4,14 +4,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import ru.sergalas.FirstSecurity.security.PersonDetails;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "hello";
+
+        return "site/hello";
     }
 
     @GetMapping("/showUserInfo")
@@ -20,6 +20,6 @@ public class HelloController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         PersonDetails personDetails =(PersonDetails) authentication.getPrincipal();
         System.out.println(personDetails.getPerson());
-        return "hello";
+        return "site/hello";
     }
 }
