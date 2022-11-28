e77b39f05903ae98aa64143324935ca0db15544a
sergalas <hudos@rambler.ru>
2022-11-28 21:16:32 +0300

    add roles

--- src/main/java/ru/sergalas/FirstSecurity/controllers/HelloController.java
+++ src/main/java/ru/sergalas/FirstSecurity/controllers/HelloController.java
@@ -1,13 +1,19 @@
 package ru.sergalas.FirstSecurity.controllers;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.sergalas.FirstSecurity.security.PersonDetails;
+import ru.sergalas.FirstSecurity.services.AdminServices;
 
 @Controller
 public class HelloController {
+    private final  AdminServices adminServices;
+    @Autowired
+    public HelloController(AdminServices adminServices) {
+        this.adminServices = adminServices;
+    }
+
+
     @GetMapping("/hello")
     public String sayHello() {
         return "site/hello";
@@ -16,9 +22,7 @@ public class HelloController {
     @GetMapping("/showUserInfo")
     public String showUserInfo()
     {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        PersonDetails personDetails =(PersonDetails) authentication.getPrincipal();
-        System.out.println(personDetails.getPerson());
+        adminServices.showUserInfo();
         return "site/hello";
     }
 }
